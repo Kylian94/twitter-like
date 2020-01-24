8bb8c03e7e490d8baecedd6c8e343c919a3e3b08
Kylian <kylian.petitgenet@gmail.com>
2020-01-24 13:40:00 +0100

    first commit / add project on github

---
+++ README.md
@@ -0,0 +1,35 @@
+<p align="center"><img src="https://res.cloudinary.com/dtfbvvkyp/image/upload/v1566331377/laravel-logolockup-cmyk-red.svg" width="400"></p>
+
+<p align="center">
+<a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/d/total.svg" alt="Total Downloads"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/v/stable.svg" alt="Latest Stable Version"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/license.svg" alt="License"></a>
+</p>
+
+## Twitter Like - Laravel 6 - VueJs - Bootstrap 4
+Project of the third year at Webstart, Paris.
+
+Developped with laravel 6 (Back-End) - VueJs/Bootstrap 4 (Front-End)
+
+![twitter img](http://img.kp-dev.fr/twitterLike.png) 
+
+---
+
+### How to use
+
+- Clone the repository with __git clone__
+
+Start :
+
+- 
+
+---
+
+### License
+
+Please use and re-use however you want.
+
+---
+
+Kylian P.
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<section>
+    <div class="d-flex vh-100">
+        <div class="col-6 bg-home-visitor ">
+            <div class="layer d-flex align-items-center justify-content-center">
+                    <div class="d-flex flex-column justify-content-center">
+                            <div class="d-flex align-items-center">
+                                    <i class="fa fa-search text-white fa-3x mr-4"></i>
+                                    <h5 class="text-white font-weight-bold">Suivez vos idées.</h5>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <i class="fa fa-users text-white fa-3x mr-4"></i>
+                               <h5 class="text-white font-weight-bold">Découvrez ce dont les gens parlent.</h5>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <i class="fa fa-comment text-white fa-3x mr-4"></i>
+                                <h5 class="text-white font-weight-bold">Rejoignez la conversation.</h5>
+                            </div>
+                            
+                            
+                            
+                    </div>
+            </div>
+        </div>
+        <div class="col-6 d-flex align-items-center justify-content-center bg-white">
+                
+                <div class="d-flex flex-column justify-content-center col-6">
+                    <i class="fa fa-linode text-info fa-3x mr-3"></i>
+                    <h1 class="font-weight-bold mt-2">Découvrez ce qui se passe dans le monde en temps réel</h1>
+                    <h6 class="font-weight-bold mt-5">Rejoignez Twitter aujourd'hui.</h6>
+                    <a class="btn btn-info text-white font-weight-bold btn-rounded mt-3" href="{{ route('register') }}">S'inscrire</a>
+                    <a class="btn btn-outline-info font-weight-bold text-info btn-rounded mt-3" href="{{ route('login') }}">Se connecter</a>
+                </div>
+            
+            
+        </div>
+    </div>
+    
+</section>
+@endsection
