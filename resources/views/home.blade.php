f56d1fad1ccc56af9cc8c142f0528ad48cb37817
Kylian <kylian.petitgenet@gmail.com>
2020-01-28 16:42:12 +0100

    add comments DOM

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-
     <div class="col-12 col-md-6 border-left border-right main-part p-0">
         <div class="d-flex flex-column align-items-start">
+            <!-- HEADER PART -->
             <div class="px-3">
                 <div class="d-flex align-items-center justify-content-sm-between mt-2">
                     <div class="col-2 p-0 d-md-none">
@@ -16,145 +16,139 @@
                 </div>
             </div>
             
-                
             <div class="col-12 p-0">
                     <hr>
             </div>
-            
+            <!-- MOBILE BUTTON ADD TWEET + MODAL -->
             <a class="btn btn-mobile-tweet btn-info text-white fixed-bottom d-md-none d-flex align-items-center justify-content-center" data-toggle="modal" data-target="#modalTweetMobile"><i class="fa fa-bandcamp text-white fa-3x "></i></a>
             <div class="modal fade " id="modalTweetMobile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content-mobile border-0">
-                            <div class="modal-header">
-                                
-                                <button type="button" class="close p-0 m-0" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true" class="text-info">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="d-flex col-12 p-0">
-                                    <div class="col-2 p-0">
-                                        <img class="img-profil-mobile rounded-circle" src="https://images.unsplash.com/photo-1453396450673-3fe83d2db2c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="" srcset="">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content-mobile border-0">
+                        <div class="modal-header">
+                            
+                            <button type="button" class="close p-0 m-0" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true" class="text-info">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="d-flex col-12 p-0">
+                                <div class="col-2 p-0">
+                                    <img class="img-profil-mobile rounded-circle" src="https://images.unsplash.com/photo-1453396450673-3fe83d2db2c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="" srcset="">
+                                </div>
+                                <div class="col-10 d-flex flex-column p-0">
+                                    <div class="col-12 p-0">
+                                        <textarea class="inputTweetMobile col-12 p-0"type="text" placeholder="Quoi de beau aujourd'hui ?" autofocus></textarea>
                                     </div>
-                                    <div class="col-10 d-flex flex-column p-0">
-                                        <div class="col-12 p-0">
-                                            <textarea class="inputTweetMobile col-12 p-0"type="text" placeholder="Quoi de beau aujourd'hui ?" autofocus></textarea>
+                                </div>
+                            </div>
+                            <div class="d-flex col-12 align-items-center mt-3">
+                                <div class="col-2">
+                                
+                                </div>
+                                <div class="col-10 d-flex flex-column p-0">
+                                    <div class="w-100 d-flex justify-content-between align-items-center">
+                                        <div class="col-3 d-flex align-items-center p-0">
+                                            <i class="fa fa-picture-o text-info mr-3 fa-2x" aria-hidden="true"></i>
+                                            <i class="fa fa-smile-o text-info fa-2x" aria-hidden="true"></i>
+                                        </div>
+                                        <div class="col-3 mr-4">
+                                            <a href="#" class="btn btn-info text-white btn-rounded px-4">Tweet</a>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="d-flex col-12 align-items-center mt-3">
-                                    <div class="col-2">
-                                    
-                                    </div>
-                                    <div class="col-10 d-flex flex-column p-0">
-                                        <div class="w-100 d-flex justify-content-between align-items-center">
-                                            <div class="col-3 d-flex align-items-center p-0">
-                                                <i class="fa fa-picture-o text-info mr-3 fa-2x" aria-hidden="true"></i>
-                                                <i class="fa fa-smile-o text-info fa-2x" aria-hidden="true"></i>
-                                            </div>
-                                            <div class="col-3 mr-4">
-                                                    <a href="#" class="btn btn-info text-white btn-rounded px-4">Tweet</a>
-                
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- TWEET ACTION PART HOME -->
-                <span class="d-none d-md-block col-12 p-0">
-                    <div class="d-flex col-12 px-3">
-                        <div class="col-2 p-0 ml-2">
-                            <img class="img-profil rounded-circle" src="https://images.unsplash.com/photo-1453396450673-3fe83d2db2c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="" srcset="">
-                        </div>
-                        <div class="col-10 d-flex flex-column p-0 mt-2">
-                            <div class="col-12 p-0">
-                                <textarea class="inputTweet col-12 p-0" autofocus type="text" placeholder="Quoi de beau aujourd'hui ?"></textarea>
-                            </div>
-                        </div>
+            </div>
+            <!-- ADD TWEET DESKTOP PART HOME -->
+            <span class="d-none d-md-block col-12 p-0">
+                <div class="d-flex col-12 px-3">
+                    <div class="col-2 p-0 ml-2">
+                        <img class="img-profil rounded-circle" src="https://images.unsplash.com/photo-1453396450673-3fe83d2db2c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="" srcset="">
                     </div>
-                    <div class="d-flex col-12 align-items-center mt-3 border-bottom ">
-                        <div class="col-2">
-                        
-                        </div>
-                        <div class="col-11 d-flex flex-column p-0 ">
-                            <div class="w-100 d-flex justify-content-between align-items-center mb-3">
-                                <div class="col-3 d-flex align-items-center p-0">
-                                    <i class="fa fa-picture-o text-info mr-3 fa-2x" aria-hidden="true"></i>
-                                    <i class="fa fa-smile-o text-info fa-2x" aria-hidden="true"></i>
-                                </div>
-                                <div class="col-3">
-                                        <a href="#" class="btn btn-info text-white btn-rounded px-4">Tweet</a>
-    
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </span>
-                <!-- TWEETS LIST PART HOME -->
-                <div class="border-bottom post rounded">
-                    <div class=" d-flex flex-column align-items-center justify-content-center col-12 mt-3 rounded">
-                        <div class="d-flex justify-content-between col-12 pt-3">
-                            <div class="titleCategory d-flex ">
-                                <i class="fa fa-bandcamp text-info fa-3x mr-3"></i>
-                                <div class="d-flex flex-column align-items-start justify-content-center">
-                                    <h4 class="m-0">Twitter Music</h4>
-                                    <p class="font-weight-light">@Twitter</p>
-                                </div>
-                            </div>
-                            <i class="fa fa-bandcamp text-info fa-2x mr-3"></i>
-                        </div>
-                        <div class="col-10">
-                            <p class="font-weight-light">Lorem ipsum dolor, sit amet consectetur <span class="text-info">@adipisicing elit</span>. Minima sit repudiandae vero necessitatibus laudantium repellat mollitia.</p>
-                            <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1579833214916-783b168e55d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3360&q=80" alt="" srcset="">
-                            
-                            <div class="my-2 d-flex align-items-center font-weight-light col-12">
-                                <p class="m-0 small w-75">Il y à 2 jours</p>
-                                <i class="fa fa-heart-o text-info fa-1x ml-4 mr-1"></i>
-                                <p class="m-0 w-75">31,4 k</p>
-                                
-                                <i class="fa fa-comment-o text-info fa-1x ml-3 mr-2"></i>
-                                <p class="m-0">33</p>
-                            </div>
+                    <div class="col-10 d-flex flex-column p-0 mt-2">
+                        <div class="col-12 p-0">
+                            <textarea class="inputTweet col-12 p-0" autofocus type="text" placeholder="Quoi de beau aujourd'hui ?"></textarea>
                         </div>
                     </div>
                 </div>
+                <div class="d-flex col-12 align-items-center mt-3 border-bottom ">
+                    <div class="col-2">
+                    
+                    </div>
+                    <div class="col-11 d-flex flex-column p-0 ">
+                        <div class="w-100 d-flex justify-content-between align-items-center mb-3">
+                            <div class="col-3 d-flex align-items-center p-0">
+                                <i class="fa fa-picture-o text-info mr-3 fa-2x" aria-hidden="true"></i>
+                                <i class="fa fa-smile-o text-info fa-2x" aria-hidden="true"></i>
+                            </div>
+                            <div class="col-3">
+                                    <a href="#" class="btn btn-info text-white btn-rounded px-4">Tweet</a>
 
-                <div class="border-bottom post rounded mb-5">
-                    <div class=" d-flex flex-column align-items-center justify-content-center col-12 mt-3 rounded">
-                        <div class="d-flex justify-content-between col-12 pt-3">
-                            <div class="titleCategory d-flex ">
-                                <i class="fa fa-bandcamp text-info fa-3x mr-3"></i>
-                                <div class="d-flex flex-column align-items-start justify-content-center">
-                                    <h4 class="m-0">Twitter Music</h4>
-                                    <p class="font-weight-light">@Twitter</p>
-                                </div>
-                            </div>
-                            <i class="fa fa-bandcamp text-info fa-2x mr-3"></i>
-                        </div>
-                        <div class="col-10">
-                            <p class="font-weight-light">Lorem ipsum dolor, sit amet consectetur <span class="text-info">@adipisicing elit</span>. Minima sit repudiandae vero necessitatibus laudantium repellat mollitia.</p>
-                            <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1579833214916-783b168e55d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3360&q=80" alt="" srcset="">
-                            
-                            <div class="my-2  d-flex align-items-center font-weight-light col-12">
-                                <p class="m-0 small w-75">Il y a 2 jours</p>
-                                <i class="fa fa-heart-o text-info fa-1x ml-4 mr-1"></i>
-                                <p class="m-0 w-75">31,4 k</p>
-                                
-                                <i class="fa fa-comment-o text-info fa-1x ml-3 mr-2"></i>
-                                <p class="m-0">33</p>
                             </div>
                         </div>
-                        
                     </div>
                 </div>
+            </span>
+            <!-- TWEETS LIST PART HOME -->
+            <div class="border-bottom post rounded">
+                <div class=" d-flex flex-column align-items-center justify-content-center col-12 mt-3 rounded">
+                    <div class="d-flex justify-content-between col-12 pt-3">
+                        <div class="titleCategory d-flex ">
+                            <i class="fa fa-bandcamp text-info fa-3x mr-3"></i>
+                            <div class="d-flex flex-column align-items-start justify-content-center">
+                                <h4 class="m-0">Twitter Music</h4>
+                                <p class="font-weight-light">@Twitter</p>
+                            </div>
+                        </div>
+                        <i class="fa fa-bandcamp text-info fa-2x mr-3"></i>
+                    </div>
+                    <div class="col-10">
+                        <p class="font-weight-light">Lorem ipsum dolor, sit amet consectetur <span class="text-info">@adipisicing elit</span>. Minima sit repudiandae vero necessitatibus laudantium repellat mollitia.</p>
+                        <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1579833214916-783b168e55d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3360&q=80" alt="" srcset="">
+                        
+                        <div class="my-2 d-flex align-items-center font-weight-light col-12">
+                            <p class="m-0 small w-75">Il y à 2 jours</p>
+                            <i class="fa fa-heart-o text-info fa-1x ml-4 mr-1"></i>
+                            <p class="m-0 w-75">31,4 k</p>
+                            
+                            <i class="fa fa-comment-o text-info fa-1x ml-3 mr-2"></i>
+                            <p class="m-0">33</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-        </div>
-        
+            <div class="border-bottom post rounded mb-5">
+                <div class=" d-flex flex-column align-items-center justify-content-center col-12 mt-3 rounded">
+                    <div class="d-flex justify-content-between col-12 pt-3">
+                        <div class="titleCategory d-flex ">
+                            <i class="fa fa-bandcamp text-info fa-3x mr-3"></i>
+                            <div class="d-flex flex-column align-items-start justify-content-center">
+                                <h4 class="m-0">Twitter Music</h4>
+                                <p class="font-weight-light">@Twitter</p>
+                            </div>
+                        </div>
+                        <i class="fa fa-bandcamp text-info fa-2x mr-3"></i>
+                    </div>
+                    <div class="col-10">
+                        <p class="font-weight-light">Lorem ipsum dolor, sit amet consectetur <span class="text-info">@adipisicing elit</span>. Minima sit repudiandae vero necessitatibus laudantium repellat mollitia.</p>
+                        <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1579833214916-783b168e55d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3360&q=80" alt="" srcset="">
+                        
+                        <div class="my-2  d-flex align-items-center font-weight-light col-12">
+                            <p class="m-0 small w-75">Il y a 2 jours</p>
+                            <i class="fa fa-heart-o text-info fa-1x ml-4 mr-1"></i>
+                            <p class="m-0 w-75">31,4 k</p>
+                            
+                            <i class="fa fa-comment-o text-info fa-1x ml-3 mr-2"></i>
+                            <p class="m-0">33</p>
+                        </div>
+                    </div>   
+                </div>
+            </div>
+        </div>   
     </div>
-    
 
 
 @endsection
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,61 +3,62 @@
 @section('content')
 
     <div class="col-12 col-md-6 main-part border-left border-right p-0">
+        <!-- HEADER PART -->
         <div class="px-3">
             <div class="d-flex align-items-center mt-3">
-                    <a href="/">
-                        <i class="fa fa-arrow-left text-info fa-2x mr-3"></i>
-                    </a>
-                    
-                    <h3 class="font-weight-bold">Kylian</h3>
+                <a href="/">
+                    <i class="fa fa-arrow-left text-info fa-2x mr-3"></i>
+                </a>
+                <h3 class="font-weight-bold">Kylian</h3>
             </div>
             <p class="font-weight-light ml-5">0 tweet</p>
         </div>
-        
-        
+        <!-- PROFILE PART -->
         <div class="col-12 banner-profile">
             <img class="img-cover" src="" alt="" srcset="">
         </div>
         <div class="px-3">
             <div class="col-12 d-flex align-items-end justify-content-between edit-profile">
+                <!-- ICON USER PART + BTN EDIT PROFILE-->
                 <div class="col-md-2 col-3 p-0">
                     <img class="img-profil-user img-thumbnail rounded-circle" src="https://images.unsplash.com/photo-1453396450673-3fe83d2db2c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="" srcset="">
                 </div>
                 <a href="#" class="btn btn-outline-info btn-rounded text-info mr-4 font-weight-bold px-4" data-toggle="modal" data-target="#modalEditProfile">Edit profile</a>
             </div>
+            <!-- MODAL EDIT PROFILE PART -->
             <div class="modal fade " id="modalEditProfile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content border-0">
                         <div class="modal-header">
-                            
                             <button type="button" class="close p-0 m-0" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true" class="text-info">&times;</span>
+                                <span aria-hidden="true" class="text-info">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body">
-                                <form>
-                                    <div class="form-row">
-                                        <div class="form-group col-md-6">
-                                        <label for="name">Nom</label>
-                                        <input type="text" class="form-control" name="name" id="name" placeholder="Nom" value="{{Auth::user()->name}}">
-                                        </div>
-                                        <div class="form-group col-md-6">
-                                        <label for="email">Adresse email</label>
-                                        <input type="email" class="form-control" id="email" name="email" placeholder="Adresse email" value="{{Auth::user()->email}}">
-                                        </div>
+                            <form>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                    <label for="name">Nom</label>
+                                    <input type="text" class="form-control" name="name" id="name" placeholder="Nom" value="{{Auth::user()->name}}">
                                     </div>
-                                    <div class="form-group">
-                                        <label for="password">Mot de passe</label>
-                                        <input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe" value="{{Auth::user()->password}}">
+                                    <div class="form-group col-md-6">
+                                    <label for="email">Adresse email</label>
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="Adresse email" value="{{Auth::user()->email}}">
                                     </div>
-                                    <button type="submit" class="btn btn-info btn-rounded text-white">Modifier</button>
-                                </form>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Mot de passe</label>
+                                    <input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe" value="{{Auth::user()->password}}">
+                                </div>
+                                <button type="submit" class="btn btn-info btn-rounded text-white">Modifier</button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
+            <!-- USER INFORMATIONS -->
             <div class="mt-5">
-                <h4 class="font-weight-bold pt-3 m-0">Kylian</h4>
+                <h4 class="font-weight-bold pt-3 m-0">{{Auth::user()->name}}</h4>
                 <p class="font-weight-light">@KylianP</p>
                 <div class="d-flex align-items-center">
                         <i class="fa fa-calendar fa-1x" aria-hidden="true"></i>
@@ -69,7 +70,7 @@
                 </div>
             </div>
         </div>
-        
+        <!-- NAV PANEL TAB PART-->
         <ul class="nav nav-tabs mt-4" id="myTab" role="tablist">
             <li class="nav-item col-4 p-0 text-center">
                 <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Tweet</a>
@@ -81,7 +82,9 @@
                 <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Likes</a>
             </li>
         </ul>
+        <!-- PANEL TAB PART-->
         <div class="tab-content mb-5 " id="myTabContent">
+            <!-- TWEET USER TAB PART-->
             <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
                 <div class="border-bottom post rounded">
                     <div class=" d-flex flex-column align-items-center justify-content-center col-12 pt-3 rounded">
@@ -111,6 +114,7 @@
                     </div>
                 </div>
             </div>
+            <!-- RETWEET USER TAB PART-->
             <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
                 <div class="border-bottom post rounded">
                     <div class=" d-flex flex-column align-items-center justify-content-center col-12 pt-3 rounded">
@@ -140,6 +144,7 @@
                     </div>
                 </div>
             </div>
+            <!-- LIKE PANEL TAB PART-->
             <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
                 <div class="border-bottom post rounded">
                     <div class=" d-flex flex-column align-items-center justify-content-center col-12 pt-3 rounded">
